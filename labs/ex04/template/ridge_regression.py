ccafbd7b02942047a4924c5430c6964fc7c2d95a
brunomaga <brunomaga@gmail.com>
2016-10-20 14:30:32 +0200

    added missing normalization to lambda

--- labs/ex04/template/ridge_regression.py
+++ labs/ex04/template/ridge_regression.py
@@ -10,5 +10,6 @@ def ridge_regression(y, tx, lamb, degree):
     """implement ridge regression."""
     #same as before, we just added +lamb*np.identity
     #remember: dimensionality is M+1!
-    w = np.dot(np.linalg.inv(np.dot(tx.T,tx)+lamb*np.identity(degree+1)) , np.dot(tx.T,y))
+    lamb_prime = lamb*2*len(y)
+    w = np.dot(np.linalg.inv(np.dot(tx.T,tx)+lamb_prime*np.identity(degree+1)) , np.dot(tx.T,y))
     return w
